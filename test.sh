2b69f081d80f97e31df8f7ebe17765da349731ee
Thomaltarix <thomas.boue@epitech.eu>
2023-03-31 11:26:58 +0200

    :bug: Fix workflow file syntax

--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
-coding-style $(pwd) $(pwd)
+input=$(pwd)/coding-style-reports.log
 
-cat coding-style-reports.log | while read line
+while read line
 do
-    echo ::error::failure
-    done
+    error_type=$(echo $line | cut -d ":" -f 3 | cut -d " " -f 2)
+    file_name=$(echo $line | cut -d ":" -f 1)
+    file_path=$(echo $line | cut -d ':' -f 1)
+    error_line=$(echo $line | cut -d ':' -f 2)
+    error_code=$(echo $line | cut -d ':' -f 4)
+    echo echo "::error file=$file_name,line=$error_line::$error_type coding style error: $file_name $error_code"
+done < $input

8b1f1cfbccb51c71ffa5ab1e51d974528d951198
Thomaltarix <thomas.boue@epitech.eu>
2023-03-31 14:45:07 +0200

    :bug: Fix add main_test.c

--- tests/main_test.c
+++ tests/main_test.c
@@ -4,3 +4,12 @@
 ** File description:
 ** main test for test
 */
+
+#include <criterion/criterion.h>
+#include <criterion/redirect.h>
+
+Test(my_printf, test1, .init = redirect_all_std)
+{
+    my_printf("Hello World!");
+    cr_assert_stdout_eq_str("Hello World!");
+}
